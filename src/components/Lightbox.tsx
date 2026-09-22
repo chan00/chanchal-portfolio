6528d94d351d19817b860fb0210248f50a31dfec
Chanchal Bhakat <chanchal.bhakat45@gmail.com>
2026-09-23 00:43:00 +0530

    fix: replace deprecated gsap-trial with official free gsap packages

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -9,7 +9,7 @@ import {
 } from "react-icons/md";
 import { smoother } from "./Navbar";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import "./styles/Lightbox.css";
 
 interface LightboxProps {
